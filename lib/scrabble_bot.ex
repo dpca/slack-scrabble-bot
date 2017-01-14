026f665cd79cab5ff77810768ccde9b2027c75e2
Dan Cahoon <dpcahoon@gmail.com>
2017-01-14 14:19:45 -0500

    Initial commit

---
+++ lib/scrabble_bot.ex
@@ -0,0 +1,18 @@
+defmodule ScrabbleBot do
+  @moduledoc """
+  Documentation for ScrabbleBot.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ScrabbleBot.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/scrabble_bot_test.exs
@@ -0,0 +1,8 @@
+defmodule ScrabbleBotTest do
+  use ExUnit.Case
+  doctest ScrabbleBot
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
