057d0f85fdfc0f41efb3af0f730155179ba3cdc2
Dan Cahoon <dpcahoon@gmail.com>
2017-01-15 19:40:16 -0500

    Working scrabble tallying

--- lib/scrabble_bot/slack.ex
+++ lib/scrabble_bot/slack.ex
@@ -1,4 +1,8 @@
 defmodule ScrabbleBot.Slack do
+  @moduledoc """
+  Handles interaction with slack real time messaging
+  """
+
   use Slack
 
   def handle_connect(slack, state) do
@@ -7,7 +11,12 @@ defmodule ScrabbleBot.Slack do
   end
 
   def handle_event(message = %{type: "message"}, _slack, state) do
-    IO.puts "Got message #{message.text}"
+    case ScrabbleBot.tally(message.text) do
+      x when x > 0 ->
+        IO.puts "Got message '#{message.text}', worth #{x} points"
+      _ ->
+        IO.puts "Got message '#{message.text}'"
+    end
     {:ok, state}
   end
   def handle_event(_, _, state), do: {:ok, state}
--- mix.exs
+++ mix.exs
@@ -2,12 +2,14 @@ defmodule ScrabbleBot.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :scrabble_bot,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :scrabble_bot,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
@@ -29,6 +31,9 @@ defmodule ScrabbleBot.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:slack, "~> 0.9.2"}]
+    [
+      {:slack, "~> 0.9.2"},
+      {:credo, "~> 0.5", only: [:dev, :test]}
+    ]
   end
 end
